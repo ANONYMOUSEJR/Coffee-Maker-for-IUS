f675ccc8edcb145d694077663ec5f001157d9ce3
Cain, the son of Adam <118769955+Robert-Oppenheimer@users.noreply.github.com>
2022-12-29 11:18:28 +0100

    - Commenting

--- Coffee Maker for IUS.cpp
+++ Coffee Maker for IUS.cpp
@@ -1,4 +1,5 @@
 // TryCoffe1.cpp : First version of the file, started on 10.12.2022. (December 12th 2022)
+// Coffee_Maker_for_IUS.cpp : Last version of the file, ended on __.12.2022. (December __ 2022)
 // Team members: - Ammar Omerika     220302227 SE
 //               - Selma Alic        220302129 CSE
 //               - Omer Becic        220302287 SE
@@ -18,27 +19,28 @@ using std::string;
 
 int main()
 {
-    char setModeOrUserMode;
-    char endOfTheDay;
-    char settingModeAction;
-    char continueSettingMode;
+    //FUNCTIONAL VARIABLES
+    char setModeOrUserMode;   //Which mode to use
+    char endOfTheDay;         //Finnish the machine
 
+    //COFFEE VARIABLES
     string coffeeNames[5] = { "Espresso", "Cappucino", "Hot Chocolate", "Melange", "Americano"};
-    int coffeeCups[5] = { 3,3,3,3,3 }; //Number of each coffee            
-    double coffeePrices[5] = { 2,2,3.5,3,1.5 }; //Prices of coffees       
-    int CoffeePosition; //Index of choosen coffe
-    double Change; //Change that will be given back
+    int coffeeCups[5] = { 3,3,3,3,3 };            //Number of each coffee            
+    double coffeePrices[5] = { 2,2,3.5,3,1.5 };   //Prices of coffees       
+    int CoffeePosition;                           //Index of choosen coffe
+    double Change;                                //Change that will be given back
     
-    const long double coinsValue[7] = { 0.05,0.1,0.2,0.5,1,2,5 }; //Value of coins
-    int coinsAmount[7] = {3,3,3,3,3,3,3}; //Amount of coins
-    bool coinsAllowed[7] = {true, true, true, true, true, true, true}; //Acceptable coins
+    //COIN VARIABLES
+    const long double coinsValue[7] = { 0.05,0.1,0.2,0.5,1,2,5 };        //Value of coins
+    int coinsAmount[7] = {3,3,3,3,3,3,3};                                //Amount of coins
+    bool coinsAllowed[7] = {true, true, true, true, true, true, true};   //Acceptable coins
 
     do {
-        start:
-        //WHICH MODE:
-        cout << "Do you wish the use the setting mode or operation mode <s/o>" << std::endl;
+        //MODE MENU
+        oasis1:
+        cls();
+        cout << "Do you wish the use the setting mode or operation mode <s/o>" << endl;
         cin >> setModeOrUserMode;
-
         if (setModeOrUserMode == 's') {
             //SETTING MODE
             cout << "SETTING MODE:" << endl;
@@ -58,21 +60,25 @@ int main()
         }
         else {
             cout << "Invalid input" << endl;
-            exit(1);
+            pause();
+            goto oasis1;
         }
 
-        //USE IT MORE OR NOT:
+        //CONTINUE OR NOT
+        oasis2:
         cout << "Do you wish to use the machine more? <y/n>" << endl;
         cin >> endOfTheDay;
         if (endOfTheDay == 'y') {
             cout << endl;
         }
         else if (endOfTheDay == 'n') {
+            cout << endl;
             break;
         }
         else {
             cout << "Invalid input" << endl;
-            exit(1);
+            pause();
+            goto oasis2;
         }
 
     } while (true);
@@ -80,7 +86,7 @@ int main()
 
     //END OF THE DAY EVALUATION
     cout << "END OF THE DAY EVALUATION: " << endl;
-    // Test.
+    cout << "Look for it in the text file attached ;)" << endl;
     return 0;
 }
 /*
